ea7156c5016a8e012f209525358e851263594908
Pierre CAILLON <pierre.caillon@edu.devinci.fr>
2023-03-12 19:44:24 +0100

    add infos to the footer

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, html, dcc
-from pathlib import Path
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
+
+from pathlib import Path
+from dash import Dash, html, dcc
 
 
 app = Dash(__name__)
@@ -18,19 +16,25 @@ HISTORY_PATH = Path().absolute() / "history.csv"
 def serve_layout():
     df = pd.read_csv(HISTORY_PATH)
     df["date"] = pd.to_datetime(df["timestamp"], unit="s")
-    fig = px.line(df, x="date", y="count")
+    fig = px.line(df, x="date", y="count", title="Concurrent viewers over time")
+    fig.update_layout(title_x=0.5)
 
     return html.Div(children=[
-        html.H1('Twitch viewership'),
+        html.H1('Twitch viewership', className="header-title", style={'textAlign': 'center'}),
 
         html.Div([f"Live viewers now: ", html.B(df['count'].iloc[-1])]),
 
-        dcc.Graph(
-            id='view-count',
-            figure=fig
-        ),
+        dcc.Graph(figure=fig),
 
-        html.I('Made by Pierre CAILLON'),
+        html.Footer(
+            html.I([
+                'Website made by Pierre CAILLON. Data scrapped from ',
+                html.A("TwitchTracker", href="https://twitchtracker.com"),
+                '. View source code on ',
+                html.A("GitHub", href="https://github.com/pierrecaillon/twitch_viewers"),
+                '.',
+            ])
+        ),
     ])
 
 
